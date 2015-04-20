e5f362300505ef5d88262048dfed34c836711900
MarenS2 <maren-sitte@t-online.de>
2015-04-20 10:33:55 +0200

    Quiz 2

---
+++ Quiz2_Developing_Data_Products.R
@@ -0,0 +1,25 @@
+## Quiz 2
+# Question 1
+
+# In the slidify YAML text. Changing the framework from io2012 to shower does what?
+# It changes the html5 framework thus changing the style of the slides.
+
+# Question 2.
+
+```{r, echo=TRUE, results='hide'}
+# only shows the scripts
+
+```
+
+
+# Question 3
+
+```{r, echo=FALSE}
+# only shows the output results
+```
+
+# Question 5
+
+```{r, echo=TRUE, eval=FALSE}
+# only shows the scripts
+```
